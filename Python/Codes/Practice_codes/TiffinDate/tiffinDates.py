1474ffe72f1ebb7aacb2182066fdd3a3e3a576d5
Romit Thete <romit.avishkar@gmail.com>
2023-08-03 20:28:28 +0200

    Updated tiffinDates.py, added a bat file to run it on Windows, added .gitignore

--- Python/Codes/Practice_codes/TiffinDate/tiffinDates.py
+++ Python/Codes/Practice_codes/TiffinDate/tiffinDates.py
@@ -1,7 +1,7 @@
+import argparse
 import sys
 from datetime import date
 from datetime import timedelta
-
 
 day_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
@@ -22,14 +22,37 @@ def tiffin_next_date(next_start_date, given_delta):
     return date_today, next_start_date
 
 
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        start_date = input("Enter start date (YYYY-MM-DD): ")
-        delta = input("Enter the difference: ")
-    else:
-        start_date = sys.argv[1]
-        delta = sys.argv[2]
+def CustomParser():
+    # Custom parser to parse command line arguments using argparse package.
+    # Three steps:
+    ## 1. Initialize the parser
+    ## 2. Add custom arguments
+    ## 3. Parse the arguments
+    parser = argparse.ArgumentParser(
+        prog="tiffinDates.py",
+        description="Get the last date and next start date (also the pay date) of Tiffin service",
+        epilog="Thanks for using! Developed by Romit!"
+    )
 
+    # 2. Adding arguments
+    # kwargs used:
+    ## metavar - Optional name to be displayed in -h
+    ## help - Help message to be displayed in -h
+    ## default - Can use to set default values (not used here)
+    parser.add_argument('-sd', '--start-date', metavar="", help="Start date of the current month")
+    parser.add_argument('-d', '--delta', metavar="", help="Difference in start and end date (in days)")
+
+    # 3. Parsing the args
+    args = parser.parse_args()
+    return args.start_date, args.delta
+
+
+if __name__ == "__main__":
+    start_date, delta = CustomParser()
+    if start_date is None:
+        start_date = input("Enter start date (YYYY-MM-DD): ")
+    if delta is None:
+        delta = input("Enter the difference: ")
     print("Start date for this month: {}".format(start_date))
     end_date, pay_date = tiffin_next_date(start_date, delta)
     print("Day: {}: {}".format(delta, end_date))
